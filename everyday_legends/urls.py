e4e1e0ab45f0f224f5e1c3a74679120f874a1cbd
Welshy92 <>
2023-04-14 10:35:59 +0000

    Set up index view

--- everyday_legends/urls.py
+++ everyday_legends/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from legends import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.base, name='base')
 ]
