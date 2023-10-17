f25e8280b3edfa4c611dfb9bf93987617eff7ee8
Jacob Welsh <welshy92@outlook.com>
2023-10-17 14:08:12 +0000

    CHANGE external hostname in settings

--- everyday_legends/settings.py
+++ everyday_legends/settings.py
@@ -42,7 +42,9 @@ ALLOWED_HOSTS = [
     'everyday-legends.onrender.com'
     ]
 
-RENDER_EXTERNAL_HOSTNAME = 'https://everyday-legends.onrender.com'
+ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
